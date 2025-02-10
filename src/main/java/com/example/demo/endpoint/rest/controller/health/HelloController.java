5f9b65b8ac215615651df38425e3877a880e00c9
Lovatiana <177833276+LovatianaSoa@users.noreply.github.com>
2025-02-10 16:09:42 +0300

    chore: delete syntaxe

--- src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/HelloController.java
@@ -1,6 +1,4 @@
 package com.exercice.demo.endpoint.rest.controller.health;
-
-import com.exercice.demo.PojaGenerated;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
